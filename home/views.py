43559b7f92e7b4ffd35d493753b3dfc3cfe29e50
Saurav <sp.gharti@gmail.com>
2018-04-02 17:57:53 +0000

    add package django-paypal2

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-paypal2', 'url': 'http://pypi.python.org/pypi/django-paypal2/1.0.2'},
 	{'name':'django-paypal', 'url': 'http://pypi.python.org/pypi/django-paypal/0.4.1'},
     ]
     context = {
